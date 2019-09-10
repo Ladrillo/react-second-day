cc70c2234f7e581679089eecc1d40c45772cae76
Gabriel Cabrejas <gabriel@cabrejas.net>
2019-09-10 11:19:48 +0200

    wip

--- src/App.js
+++ src/App.js
@@ -14,27 +14,33 @@ import './App.css';
 
 function Fruit(props) { // declaration of the Fruit component
   // what properties do we need inside of data arg, to flesh out our fruit
-  // const { name, addToCart } = props;
+  const { name, addToCart } = props;
   return (
     <div>
-      <h3>{props.name}</h3>
-      <button onClick={props.addToCart}>Add To Cart</button>
+      <h3>{name}</h3>
+      <button onClick={addToCart}>Add To Cart</button>
     </div>
   );
 }
 
+function Fruits(props) {
+  // what props do we need?
+  const { fruits, addToCart } = props; // ['apple', 'banana']
+  return (
+    <div>
+      {
+        fruits.map(
+          fruitName => <Fruit name={fruitName} addToCart={addToCart} />
+        )
+      }
+    </div>
+  )
+}
+
 function App() {
-
-  const callback = () => {
-    console.log('adding this fruit')
-  }
-
   return (
     <div className="App">
-      Here's a fruit:
-      {/* here is the invocation of Fruit (using JSX) */}
-      <Fruit name="pear" addToCart={callback} />
-      <Fruit name="apple" addToCart={callback} />
+
     </div>
   );
 }
